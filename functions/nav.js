a1eefb3d604ad39b08d02e08ffcf2bbdc88b2a1d
JingWay <guojingwei129@gmail.com>
2025-12-12 15:28:35 +0800

    update auth

--- functions/nav.js
+++ functions/nav.js
@@ -1,6 +1,6 @@
 export async function onRequestGet(context) {
     const appKey = context.request.headers.get('appKey');
-    const data = await context.env.GPage.get('appKey');
+    const data = await context.env.GPage.get(appKey);
     return new Response(data || '[]', {
         headers: { 'Content-Type': 'application/json' }
     });
@@ -8,12 +8,12 @@ export async function onRequestGet(context) {
 
 export async function onRequestPost(context) {
     const appKey = context.request.headers.get('appKey');
-    const data = await context.env.GPage.get('appKey');
+    const data = await context.env.GPage.get(appKey);
     if (data === null) {
         return new Response('Unauthorized', { status: 401 });
     } else {
         const body = await context.request.text();
-        await context.env.GPage.put('appKey', body);
+        await context.env.GPage.put(appKey, body);
         return new Response('OK');
     }
 }
